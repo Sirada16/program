4090201b240ae64f5a3a27bab77ebf87f1deed9a
taenggoo97@gmail.com <taenggoo97@gmail.com>
2020-06-11 21:00:39 +0700

    no 1-4 80%

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //openfile ข้อ1 อ่านไฟล์
-$text = file('word.txt');
+/*$text = file('word.txt');
 foreach($text as $index=>$value){
 	echo $value."<br>";
 
@@ -15,5 +15,25 @@ foreach($text as $index=>$value){
 				  echo "success  writing to file";
 	  }
   }
-  
+  */
+  $data = file('text.txt');
+
+$count = count($data);
+foreach($data as $index=>$value){
+$word = substr($value, 0, -2);//ตัดenterออก
+$big = strtoupper(substr($word,0,$count-($count-1)));//ตัวอักษรตัวแรก
+$big2 = substr($word,1,$count);//ตัวอักษรหลังตัวแรก
+    //if ตรวจสอบสอบว่ามีโฟลเดอร์นี้อยู่ไหม
+    if(!@mkdir($big,0,true)){
+        $path = $big;
+        $name = fopen($path."/".$word.".txt", 'w+');
+        if($name){
+          if(!fwrite($name, strtolower(str_repeat($value,100)))) {
+        		echo "success  writing to file";
+           }
+            
+            $pos = strcspn(strtolower($big2), "aeiou");//aeiouอยู่ที่indexไหนแต่ไม่ได้เริ่มด้วย0
+        }             
+    }
+}
 ?>
