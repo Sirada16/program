b52fb589aeb94bb444706c15ad8e918cdeaf001b
taenggoo97@gmail.com <taenggoo97@gmail.com>
2020-06-11 12:53:03 +0700

    update

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ $text = file('word.txt');
 foreach($text as $index=>$value){
 	echo $value."<br>";
   //write file
-$handle = fopen("$value.txt", 'w+');
-     
-        $handle = fopen("$index.txt", 'w+');
+}
+$data=file('text.txt');
+foreach($data as $index=>$value1){
+$handle = fopen("$value1.txt", 'x+');
         if($handle){
-
-        if(!fwrite($handle, str_repeat($value,100)))  die("couldn't write to file."); 
-        echo "success  writing to file";
-}  
-    }
+        	if(!fwrite($handle, strtolower(str_repeat($value1,100)))) 
+        		echo "success  writing to file";
+		} 
+}
 ?>
