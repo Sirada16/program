47b5275ff6b0f16a8a602251e55b846eed660eef
taenggoo97@gmail.com <taenggoo97@gmail.com>
2020-06-11 10:46:23 +0700

    add new

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 $text = file('word.txt');
 foreach($text as $index=>$value){
 	echo $value."<br>";
-    
+   
+$handle = fopen("$value.txt", 'w+');
+     
+        $handle = fopen("$index.txt", 'w+');
+        if($handle){
+
+        if(!fwrite($handle, str_repeat($value,100)))  die("couldn't write to file."); 
+        echo "success  writing to file";
 }
+    }
 ?>
