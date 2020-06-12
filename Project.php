f3b180bcd9d2155c2f8c0001254f02192e5667c5
taenggoo97@gmail.com <taenggoo97@gmail.com>
2020-06-12 13:00:58 +0700

    update

--- Project.php
+++ Project.php
@@ -18,23 +18,22 @@ foreach($text as $index=>$value){
   */
   //ข้อ 1-4
   $data = file('word.txt');
-  $data = file('text.txt');
   $count_data = count($data);
   foreach($data as $index=>$value){
   $enter = array("\n","\r",' ');
   $value = strtolower(str_replace($enter,'',$value));
-  $first_word = substr($value,0,$count_data-($count_data-1));//ตัวอักษรตัวแรก
+  $first_word = strtoupper(substr($value,0,$count_data-($count_data-1)));//ตัวอักษรตัวแรก
   $word_after_first = substr($value,1,$count_data);//ตัวอักษรหลังตัวแรก
-  $check_vowel = strcspn(strtolower($value), "aeiou");//aeiouอยู่ที่indexไหน
-  $vowel = substr($value,$check_vowel,1);//สระ
+  $check_vowel = strcspn(strtolower($word_after_first), "aeiou");//aeiouอยู่ที่indexไหน
+  $vowel = strtoupper(substr($value,$check_vowel+1,1));//สระ
       //if ตรวจสอบว่ามีโฟลเดอร์นี้อยู่ไหม
       if(!@mkdir($first_word."/".$vowel,0,true)){
-                      $myfile = fopen($first_word."/".$vowel."/".$value.".txt", 'w+');
+                      $myfile = fopen($first_word."/".$vowel."/".$first_word.$word_after_first.".txt", 'w+');
                               fwrite($myfile, str_repeat($value,100));
                   }else{//สำหรับcaseที่aeiouซ้ำ
-                          $myfile = fopen($first_word."/".$vowel."/".$value.".txt", 'x+');
+                      $myfile = fopen($first_word."/".$vowel."/".$first_word.$word_after_first.".txt", 'x+');
                                   fwrite($myfile, str_repeat($value,100));
-                      }//endelse
+                      }
   
   }//endforeach
   
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+1.ทำการโหลดโปรแกรม Xampp สามารถโหลดได้ตามลิ้งก์ https://www.apachefriends.org/index.html
+2.เมื่อโหลดแลพทำการติดตั้งเสร็จสิ้น ให้ทำการโหลดไฟล์ Project.php และ word.txt นำไปติดติ้งไว้ที่ C:\xampp\htdocs เพื่อทำการ Run ไฟล์
+3.เปิดโปรแกรม Xampp ทำการกดปุ่ม Start ในส่วนของ Apache 
+4.หลังจากนั้นกดปุ่ม Admin
+5.เมื่อทำการกดปุ่ม Admin จะทำการเปิดหน้าต่าง Browser เพื่อ Run Project.php และจะแสดงผลใน Folder ที่ทำการติดตั้งไฟล์
